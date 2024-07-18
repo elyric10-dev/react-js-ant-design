04f3ac8eedf80bbbcc777087e59f6db19c230916
edl-elyric <elyric.manatad@edlution.com.sg>
2024-07-18 14:05:43 +0800

    Cleaning comments

--- src/components/CustomHeader.jsx
+++ src/components/CustomHeader.jsx
@@ -33,31 +33,3 @@ function CustomHeader({ collapsed }) {
 }
 
 export default CustomHeader;
-
-// import { Avatar, Flex, Typography } from "antd";
-// import Search from "antd/es/input/Search";
-// import React from "react";
-// import { BiMessage, BiUser } from "react-icons/bi";
-// import { IoNotifications } from "react-icons/io5";
-
-// const CustomHeader = () => {
-//   return (
-//     <Flex align="center" justify="space-between">
-//       <Typography.Title level={3} type="secondary">
-//         Welcome back, Elyric
-//       </Typography.Title>
-
-//       <Flex align="center" gap={"3rem"}>
-//         <Search placeholder="Search Dashboard" allowClear />
-
-//         <Flex align="center" gap={"10px"}>
-//           <BiMessage />
-//           <IoNotifications />
-//           <Avatar icon={<BiUser />} />
-//         </Flex>
-//       </Flex>
-//     </Flex>
-//   );
-// };
-
-// export default CustomHeader;
